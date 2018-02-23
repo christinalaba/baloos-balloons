0d29fb81944f317284d8e22c0ff36202e225db59
Christina Laba <christinalaba.m@gmail.com>
2018-02-23 11:50:08 -0500

    setup

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Transition from 'react-transition-group/Transition';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
+
+ 
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
+  constructor(props) {
+    super(props);
+    
+    this.state = {}
+    
+  }
+  
+  
+  //this starts the animation loop for all the baloons
+  play() {
+  }
+  
+  
+  //this removes balloon
+  pop() {
+    this.setState({
+      opacity: 0
+    })
+  }
+  
+  //this ads to the points tally
+  increaseCounter() {
+  }
+  
+  render() {
+    return (
+      
+      <div>
+        <div className="scoreBoard">
+          <p>Score:</p>
         </div>
-      )
-    }
+        <div className="canvas">
+          <ReactCSSTransitionGroup>
+            <img src="/dev/assets/balloon.png" alt="" className="balloon" />
+          </ReactCSSTransitionGroup>
+        </div>
+        <form>
+          <input type="submit" onClick={this.play} />
+        </form>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
